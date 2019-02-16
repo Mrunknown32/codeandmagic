ec5b0add794e0624583e3ec9d622bdfc37e2e363
mrunknown32 <mr.unknown32@mail.ru>
2019-02-16 14:11:49 +0300

    initial

---
+++ 343741-code-and-magick-master/js/render-wizards.js
@@ -0,0 +1,38 @@
+'use strict';
+(function () {
+
+  var WIZARD_QUANTITY = 4;
+
+  var renderWizard = function (wizard) {
+    var wizardElement = similarWizardTemplateElement.cloneNode(true);
+
+    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+
+    return wizardElement;
+  };
+
+  var userDialogElement = document.querySelector('.setup');
+  var similarListElement = userDialogElement.querySelector('.setup-similar-list');
+  var similarWizardTemplateElement = document.querySelector('#similar-wizard-template')
+      .content
+      .querySelector('.setup-similar-item');
+
+  var onLoadWizardsSuccess = function (wizards) {
+    var fragment = document.createDocumentFragment();
+
+    for (var i = 0; (i < WIZARD_QUANTITY || i === wizards.length); i++) {
+      fragment.appendChild(renderWizard(wizards[i]));
+    }
+    similarListElement.appendChild(fragment);
+
+    userDialogElement.querySelector('.setup-similar').classList.remove('hidden');
+  };
+
+  var onLoadWizardsError = function (error) {
+    window.errorMessage.create(error);
+  };
+
+  window.backend.load(onLoadWizardsSuccess, onLoadWizardsError);
+})();
---
+++ 343741-code-and-magick-master/js/setup.js
@@ -0,0 +1,46 @@
+'use strict';
+
+(function () {
+
+  var KEYCODE_ENTER = 13;
+
+  var setupFormElement = document.querySelector('.setup-wizard-form');
+  var setupFormOpenElement = document.querySelector('.setup-open');
+  var setupFormCloseElement = document.querySelector('.setup-close');
+  var setupFormIconElement = document.querySelector('.setup-open-icon');
+
+  setupFormIconElement.tabIndex = 0;
+  setupFormCloseElement.tabIndex = 0;
+
+  setupFormOpenElement.addEventListener('click', function () {
+    window.popup.open();
+  });
+
+  setupFormOpenElement.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === KEYCODE_ENTER) {
+      window.popup.open();
+    }
+  });
+
+  setupFormCloseElement.addEventListener('click', function () {
+    window.popup.close();
+  });
+
+  setupFormCloseElement.addEventListener('keydown', function () {
+    window.popup.close();
+  });
+
+  setupFormElement.addEventListener('submit', function (evt) {
+    window.backend.save(
+        new FormData(setupFormElement),
+        function () {
+          window.popup.close();
+        },
+        function (error) {
+          window.errorMessage.create(error);
+        });
+    evt.preventDefault();
+  });
+
+})();
+
